b4f8528d83f860728112cd156c993a08b1857e08
therahimulislam <the.rahimul.islam@gmail.com>
2025-09-14 00:08:50 +0530

    First commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   int a;
+   int b;
+   printf("\nEnter the value of a=");
+   scanf("%d",&a);
+   printf("\nEnter the value of b=");
+   scanf("%d",&b);
+   printf("\na=%d and b=%d",a,b);
+   
+   
+   
+   
+    return 0;
+}
